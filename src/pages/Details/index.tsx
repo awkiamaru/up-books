d459ec8603d8e594a7024a1b936729319102af87
Luis Fernando <luispoma.frag@gmail.com>
2020-07-05 21:48:41 -0300

    add new svg files

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import { SvgUri } from 'react-native-svg';
-
 Icon.loadFont();
 const BookDetails = () => {
   const navigation = useNavigation();
@@ -17,11 +16,18 @@ const BookDetails = () => {
         <TouchableOpacity onPress={handleNavigateBack}>
           <Icon name="x" size={35} color="#EA3F30" />
         </TouchableOpacity>
-        <SvgUri
-          width="30%"
-          height="40%"
-          uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
-        />
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.thumbnail}
+            source={{
+              uri:
+                'https://images.vexels.com/media/users/3/158004/isolated/preview/d0d467c0c649386636cd4505f265d107---cone-da---rvore-de-fruta-by-vexels.png',
+            }}
+          />
+          <View style={styles.progressContainer}>
+            <SvgUri uri="" />
+          </View>
+        </View>
       </View>
     </>
   );
@@ -32,7 +38,19 @@ export default BookDetails;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 32,
+    paddingHorizontal: 20,
     paddingTop: 20,
+  },
+  thumbnail: {
+    width: 100,
+    height: 150,
+    borderRadius: 5,
+  },
+  imageContainer: {
+    paddingTop: 20,
+    alignItems: 'center',
+  },
+  progressContainer: {
+    flexDirection: 'row',
   },
 });
